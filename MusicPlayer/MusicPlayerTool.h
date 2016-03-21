ef9c36a88cafe5c275fa83e8d4f6980d14a6b627
fanwei <fcthao@icloud.com>
2016-03-21 22:52:35 +0800

    准备播放器工具类，实现单例方法

---
+++ MusicPlayer/MusicPlayerTool.h
@@ -0,0 +1,13 @@
+//
+//  MusicPlayerTool.h
+//  MusicPlayer
+//
+//  Created by 范伟 on 16/3/21.
+//  Copyright © 2016年 FanWei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicPlayerTool : NSObject
+
+@end
---
+++ MusicPlayer/MusicPlayerTool.m
@@ -0,0 +1,13 @@
+//
+//  MusicPlayerTool.m
+//  MusicPlayer
+//
+//  Created by 范伟 on 16/3/21.
+//  Copyright © 2016年 FanWei. All rights reserved.
+//
+
+#import "MusicPlayerTool.h"
+
+@implementation MusicPlayerTool
+
+@end

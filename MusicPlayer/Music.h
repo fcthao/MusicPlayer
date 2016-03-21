6c8fc202f4bd2c3e3c6f1c738a8a98101c8e4f4a
fanwei <fcthao@icloud.com>
2016-03-21 23:53:23 +0800

    根据字典文件中的歌曲创建歌曲模型

---
+++ MusicPlayer/Music.h
@@ -0,0 +1,26 @@
+//
+//  Music.h
+//  MusicPlayer
+//
+//  Created by 范伟 on 16/3/21.
+//  Copyright © 2016年 FanWei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Music : NSObject
+/** 歌曲名称*/
+@property (nonatomic,copy)NSString *name;
+/** 专辑图片*/
+@property (nonatomic,copy)NSString *image;
+/** 歌词*/
+@property (nonatomic,copy)NSString *lrc;
+/** 带扩展名的歌曲名*/
+@property (nonatomic,copy)NSString *mp3;
+/** 歌手名称*/
+@property (nonatomic,copy)NSString *singer;
+/** 专辑的名称 */
+@property (nonatomic,copy)NSString *album;
+/** 类型*/
+@property (nonatomic,strong)NSNumber *type;
+@end
---
+++ MusicPlayer/Music.m
@@ -0,0 +1,13 @@
+//
+//  Music.m
+//  MusicPlayer
+//
+//  Created by 范伟 on 16/3/21.
+//  Copyright © 2016年 FanWei. All rights reserved.
+//
+
+#import "Music.h"
+
+@implementation Music
+
+@end
